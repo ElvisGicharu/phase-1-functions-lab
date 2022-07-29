a0bb35627821aecc3a6de3283c92cb8f4f173120
elvis <gicharuelvis@gmail.com>
2022-07-29 14:20:18 +0300

    commit tree 264fed4f79bf0d4689fa5893ac1f55137095e655

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if(block>42){
+        return block-42;
+    }else{
+        return 42-block;
+    }
+}
+function distanceFromHqInFeet(block){
+   return distanceFromHqInBlocks(block)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(destination>start){
+       return (destination-start)*264;
+    }else{
+       return (start-destination) *264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const dist= distanceTravelledInFeet(start,destination)
+    if(dist<=400){
+        return 0;
+    }else if(dist>400 && dist<=2000){
+        return (dist-400) *0.02
+    }else if(dist>2000 &&dist<=2500){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+}
+    
